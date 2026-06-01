4aa2eee9c4c8aaa829a8f5bd0c5c806963ce7b81
Miguel Correa <miguelc.dev28@gmail.com>
2026-06-01 00:08:39 -0500

    I configured the last structure of the class trip with some atributes of the class and constructors

--- src/main/java/com/empresa/transporte/model/Trip.java
+++ src/main/java/com/empresa/transporte/model/Trip.java
@@ -24,4 +24,63 @@ public class Trip {
     @JoinColumn(name = "id_vehicle")
     private Vehicle vehicle;
 
+    public Trip() {
+    }
+
+    public Trip(Long id, String origin, String destination, LocalDate departureDate, LocalDate arrivalDate, Double distanceKm) {
+        this.id = id;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureDate = departureDate;
+        this.arrivalDate = arrivalDate;
+        this.distanceKm = distanceKm;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public LocalDate getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(LocalDate arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public Double getDistanceKm() {
+        return distanceKm;
+    }
+
+    public void setDistanceKm(Double distanceKm) {
+        this.distanceKm = distanceKm;
+    }
 }
